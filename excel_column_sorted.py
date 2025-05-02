2c62aa3b6db5c7e49e8617ca67cc267c98afe044
Mike Liptack <50750360+LiptackM@users.noreply.github.com>
2025-05-02 10:21:06 -0700

    autopep8 to remove trailing whitespaces

--- excel_column_sorted.py
+++ excel_column_sorted.py
@@ -30,6 +30,7 @@ import sys
 from openpyxl import load_workbook
 from pathlib import Path
 
+
 def parse_args(argv):
     if len(argv) < 5:
         print('Usage: excel_column_sorted.py <full path to excel file> <"sheet name"> <"column title"> <"ending cell", use None if blank>')
@@ -38,7 +39,7 @@ def parse_args(argv):
     sheet_name = argv[2]
     column_title = argv[3]
     ending_cell = argv[4]
-    path = Path(initial_path) #format properly for all Operating Systems
+    path = Path(initial_path)  # format properly for all Operating Systems
     return path, sheet_name, column_title, ending_cell
 
 
@@ -59,7 +60,7 @@ def process_excel_columns_rw(filename, sheet_name):
         sheet_to_activate = workbook[sheet_name]
         workbook.active = workbook.sheetnames.index(sheet_to_activate.title)
         sheet = workbook.active
- 
+
         if sheet:
             for col in sheet.iter_cols():
                 for cell in col:
@@ -67,46 +68,46 @@ def process_excel_columns_rw(filename, sheet_name):
     finally:
         if workbook:
             workbook.close()
-            
-            
-def remove_before_and_after(my_list, front_target: str, back_target: str) ->list:
+
+
+def remove_before_and_after(my_list, front_target: str, back_target: str) -> list:
     """
     From a list composed from all cells in an excel sheet, removes
     everything before title of desired column, and then keeps everything
     else up to the ending cell.  Note that back_target is the next instance
     of that value in the list if there are multiples
-    
+
     Args:
         my_list is all the cells in the sheet by column (eg, [A1, A2, A3, B1, B2, B3])
         Front_target is the title of the column you wish to start at
         back_target is the cell after the last one you wish to keep,
             & is often blank which is noted as None
-            
+
     Return:  a trimmed list
-    
+
     Exceptions:
     if non-existence of cell contents that created this list, prints a ValueException:
       "'foo' is not in list" and then "target not found" and returns an empty list
     """
 
     try:
-        #make sure if None entered it is a NoneType
+        # make sure if None entered it is a NoneType
         back_target = None if back_target == 'None' else back_target
         front_target = None if front_target == 'None' else front_target
-        
-        #entering the same value is undefined so return empty list
-        #if front_target == back_target:
-            #return []
-        
+
+        # entering the same value is undefined so return empty list
+        # if front_target == back_target:
+        # return []
+
         front_index = my_list.index(front_target)
-        front_cut = my_list[front_index +1:]
-        
+        front_cut = my_list[front_index + 1:]
+
         back_index = front_cut.index(back_target)
-        
+
         trimmed = front_cut[:back_index]
-        
-        return trimmed  
-    
+
+        return trimmed
+
     except ValueError as value_error:
         print(value_error)
         print(f"target not found in sheet")
@@ -118,39 +119,41 @@ def process_and_sort(path, column_title, ending_cell, sheet_name):
     takes args and using these, calls process_excel_columns_rw()
     to create a list, then trims using remove_before_and_after(),
     and finally sorts the list
-    
+
     Args:
         entries from cmdline
-            
+
     Return:  final list
     """
-    initial_output, processed_list  = [], []
-    
-    #create new list 
-    for value in process_excel_columns_rw(path,sheet_name):
+    initial_output, processed_list = [], []
+
+    # create new list
+    for value in process_excel_columns_rw(path, sheet_name):
         initial_output.append(value)
-       
-    #cut the list, then sort
-    processed_list = remove_before_and_after(initial_output, column_title, ending_cell)
-    #debug
-    #if processed_list == []:
+
+    # cut the list, then sort
+    processed_list = remove_before_and_after(
+        initial_output, column_title, ending_cell)
+    # debug
+    # if processed_list == []:
     #    print("error: processed list is empty")
     processed_list.sort()
- 
+
     return processed_list
+
 
 def main():
     final = []
-    
+
     args: Tuple[Path, str, str, str] = parse_args(sys.argv)
     if args is None:
         return None
     path, sheet_name, column_title, ending_cell = args
-    
+
     final = process_and_sort(path, column_title, ending_cell, sheet_name)
-    
+
     print_output(final)
-    
-  
+
+
 if __name__ == "__main__":
     main()
